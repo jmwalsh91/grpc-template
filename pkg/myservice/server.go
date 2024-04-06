fb6006c12017678f0a4ab0f793c8be1df9b67b4a
jmwalsh91 <jordan@jmwalsh.dev>
2024-04-05 23:21:45 -0400

    running

---
+++ pkg/myservice/server.go
@@ -0,0 +1,32 @@
+package myservice
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	myservicepb "grpc-template/api/v1"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	myservicepb.UnimplementedGreetingServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *myservicepb.HelloRequest) (*myservicepb.HelloResponse, error) {
+	return &myservicepb.HelloResponse{Message: "Hello " + in.GetName()}, nil
+}
+
+func StartGRPCServer(address string) error {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	myservicepb.RegisterGreetingServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		return fmt.Errorf("failed to serve: %v", err)
+	}
+	return nil
+}
---
+++ pkg/myservice/service.go
@@ -0,0 +1,7 @@
+package myservice
+
+//boilerplate
+
+func HelloWorld() string {
+	return "Hello, World!"
+}
